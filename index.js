fe462d46f4207303db7970b40c2156c25c729221
Azolyth <azolyth@gmail.com>
2022-06-26 22:41:55 -0700

    Both random passwords functional

--- index.js
+++ index.js
@@ -3,14 +3,29 @@
 const characters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "~", "`", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "_", "-", "+", "=", "{", "[", "}", "]", ",", "|", ":", ";", "<", ">", ".", "?",
     "/"];
 
-let newPassword = [];
-
-const randomPassword = () => {
-    let random = Math.floor(Math.random() * characters.length)
-
-    newPassword.push(characters[random])
-    console.log(newPassword)
+const generatePassword = () => {
+    getPasswordOne()
+    getPasswordTwo()
 }
 
+const getPasswordOne = () => {
+    let randomPassword = '';
 
-document.getElementById('generate-btn').addEventListener('click', randomPassword)
+    for (let i = 0; i < 15; i++) {
+        let randomCharacter = Math.floor(Math.random() * characters.length);
+        randomPassword += characters[randomCharacter]
+        document.getElementById('password-one').textContent = randomPassword;
+    }
+}
+
+const getPasswordTwo = () => {
+    let randomPasswordTwo = '';
+
+    for (let i = 0; i < 15; i++) {
+        let randomCharacter = Math.floor(Math.random() * characters.length);
+        randomPasswordTwo += characters[randomCharacter]
+        document.getElementById('password-two').textContent = randomPasswordTwo;
+    }
+}
+
+document.getElementById('generate-btn').addEventListener('click', generatePassword);
