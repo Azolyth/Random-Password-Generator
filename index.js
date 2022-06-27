c5044a05bf4705f1f4f3e2f9a64b1eeaf18edd38
Azolyth <azolyth@gmail.com>
2022-06-27 04:36:17 -0700

    Setup ability to set password length

--- index.js
+++ index.js
@@ -3,15 +3,19 @@
 const characters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "~", "`", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "_", "-", "+", "=", "{", "[", "}", "]", ",", "|", ":", ";", "<", ">", ".", "?",
     "/"];
 
+const passwordLength = document.getElementById('password-length');
+const copyBtn = document.getElementById('copy-btn');
+
 const generatePassword = () => {
     getPasswordOne()
     getPasswordTwo()
+    console.log();
 }
 
 const getPasswordOne = () => {
     let randomPassword = '';
 
-    for (let i = 0; i < 15; i++) {
+    for (let i = 0; i < passwordLength.value; i++) {
         let randomCharacter = Math.floor(Math.random() * characters.length);
         randomPassword += characters[randomCharacter]
         document.getElementById('password-one').textContent = randomPassword;
@@ -21,7 +25,7 @@ const getPasswordOne = () => {
 const getPasswordTwo = () => {
     let randomPasswordTwo = '';
 
-    for (let i = 0; i < 15; i++) {
+    for (let i = 0; i < passwordLength.value; i++) {
         let randomCharacter = Math.floor(Math.random() * characters.length);
         randomPasswordTwo += characters[randomCharacter]
         document.getElementById('password-two').textContent = randomPasswordTwo;
